e3fee03756ad167344b605a04a18906fe6295cf5
Kiraa Corsac <wire150@gmail.com>
2021-01-12 13:31:07 +0100

    camera raises when detecting in unknown location

--- AlertCreators/camera.py
+++ AlertCreators/camera.py
@@ -15,7 +15,7 @@ class Camera(AlertCreator):
 
     detect_movement(what, where): void
       - call create_alert(), with what, where, level 2
-      - if where is not in self.where (camera is pointing in unknown location), raise Exception
+      - if where is not in self.possible_locations (camera is pointing in unknown location), raise Exception
       - if camera is in nightmode, and datetime hour is between 6-22 (day), do nothing
       - if camera is in nightmode, and datetime hour is between 22-6 (night), do create alert, level 3
       - if camera is NOT in nightmode, just create the alert as usual
@@ -25,9 +25,9 @@ class Camera(AlertCreator):
     (recording)
     '''
 
-    def __init__(self, name, where):
+    def __init__(self, name, possible_locations):
         super().__init__(name)
-        self.where = where
+        self.possible_locations = possible_locations
         self.night_mode = False
 
 
@@ -40,6 +40,9 @@ class Camera(AlertCreator):
      #     self.create_alert(where, what, 3)
 
     def detect_movement(self, what, where, datetime):
+        if where not in self.possible_locations:
+          raise Exception("Camera location unknown.")
+          
         currentHour = (datetime.hour)
         if self.night_mode is True:
           if 6 <= currentHour <= 22:
