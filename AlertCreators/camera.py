c7b3fa65bff49c288268f7b8e482723a12c0bbd6
jp539e <jp539e@att.com>
2021-01-12 13:01:07 +0100

    the rest

---
+++ AlertCreators/camera.py
@@ -0,0 +1,50 @@
+from AlertCreators.alert_creator import AlertCreator
+import datetime
+
+class Camera(AlertCreator):
+    '''
+    name: String
+    security_system: SecuritySystem
+      - securtiy system where the camera is registered 
+
+    where: [String]
+      - specifies where the camera could be pointnig
+
+    night_mode: Bool
+      - camera is able to create alerts during night but not during the day
+
+    detect_movement(what, where): void
+      - call create_alert(), with what, where, level 2
+      - if where is not in self.where (camera is pointing in unknown location), raise Exception
+      - if camera is in nightmode, and datetime hour is between 6-22 (day), do nothing
+      - if camera is in nightmode, and datetime hour is between 22-6 (night), do create alert, level 3
+      - if camera is NOT in nightmode, just create the alert as usual
+
+
+
+    (recording)
+    '''
+
+    def __init__(self, name, where):
+        super().__init__(name)
+        self.where = where
+        self.night_mode = False
+
+
+   # def detect_movement(self, what, where, datetime):
+     #   currentHour = (datetime.hour)
+     #   if 6 <= currentHour <= 22:
+     #     self.create_alert(where, what, 2)
+     #   else:
+     #     self.night_mode = True
+     #     self.create_alert(where, what, 3)
+
+    def detect_movement(self, what, where, datetime):
+        currentHour = (datetime.hour)
+        if self.night_mode is True:
+          if 6 <= currentHour <= 22:
+            print ("standing by")
+          else:
+            self.create_alert(where, what, 3)
+        else:
+          self.create_alert(where, what, 2)
---
+++ Tests/security_systemtest.py
@@ -0,0 +1,45 @@
+import unittest
+from unittest.mock import patch, Mock
+from AlertHandlers.doggo import Doggo
+from AlertCreators.camera import Camera
+from security_system import SecuritySystem
+
+
+class SecuritySystemTest(unittest.TestCase):
+  def setUp(self):
+    self.security = SecuritySystem()
+
+  def test_registerHandler_doggo_isInHandlers(self):
+    doggo = Doggo("Jake")
+    self.security.registerHandler(doggo)
+    self.assertIn(doggo, self.security.handlers)
+  
+  def test_registerHandler_integer_raisesTypeError(self):
+    with self.assertRaises(TypeError):
+      self.security.registerHandler(5)
+
+
+
+  @patch.object(Doggo, "handle_alert")
+  def test_alertCreated_doggoRegistred_doggoHandleAlert(self, handle_alert_mock):
+    doggo = Doggo("Jake")
+    self.security.registerHandler(doggo)
+    self.security.createAlert("outside", "human", 3)
+    handle_alert_mock.assert_called_once()
+    
+
+
+  def test_registerCreator_camera_isInCreator(self):
+    #setup
+    camera_mock = Mock()
+    
+    #act
+    self.security.registerCreator(camera_mock)
+
+    #assert
+    self.assertIn(camera_mock, self.security.creators)
+    self.assertEqual(camera_mock.security_system, self.security)
+
+  def test_registerCreator_integer_raisesTypeError(self):
+    with self.assertRaises(TypeError):
+      self.security.registerCreator(5)
