234e0aa17d12a7ce3ccdd5b121ad37815a746724
Gaeschlin, Quentin (qg218x) <qg218x@att.com>
2021-01-21 13:34:53 +0100

    filip s lesson

---
+++ Tests/integration/camera_triggers_dogtest.py
@@ -0,0 +1,20 @@
+import unittest
+from AlertCreators.camera import Camera
+from AlertHandlers.doggo import Doggo
+from security_system import SecuritySystem
+
+
+class Caemra_SecuritySystem_DoggoTest(unittest.TestCase):
+    def test_cameraAlertCreated_doggoAlertHandled(self):
+        # setup
+
+        camera = Camera("Camera2", ["outside", "garage"])
+        doggo = Doggo("Tom")
+        system = SecuritySystem()
+
+        system.registerCreator(camera)
+        system.registerHandler(doggo)
+
+        # act
+
+        # assert
