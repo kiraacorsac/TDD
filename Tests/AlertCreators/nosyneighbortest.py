7cbc6a6644a15d56b4c5060e2af0612f83fea42f
jp539e <jp539e@att.com>
2021-02-16 09:59:28 +0100

    a

--- Tests/AlertCreators/nosyneighbortest.py
+++ Tests/AlertCreators/nosyneighbortest.py
@@ -18,12 +18,12 @@ class nosyneighbortest(unittest.TestCase):
         system_create_alert_mock.assert_called_once()
 
     @patch.object(NosyNeighbor, 'create_alert')
-    def test_checksuspiciousactivity_awake_createAlert(self, create_alert_mock):
+    def test_checkSuspiciousActivity_awake_createAlert(self, create_alert_mock):
         self.nosyneighbor.check_suspicious_activity("outside", "human", datetime(2020, 11, 26, 10, 20))
         create_alert_mock.assert_called_once_with("outside", "human", 3)
 
     @patch('builtins.print')
-    def test_check_suspicious_activity_asleep_noaction(self, print_mock):
+    def test_checkSuspiciousActivity_asleep_noaction(self, print_mock):
         self.nosyneighbor.check_suspicious_activity("outside", "human", datetime(2020, 11, 26, 2, 20))
         print_mock.assert_called_once_with("Neighbor asleep")
 
