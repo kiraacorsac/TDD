476b9dd4f53baad37cea5927564f113d725f924e
Kiraa Corsac <kiraacorsac@hotmail.com>
2021-02-16 13:31:24 +0100

    integration test camera-doggo

---
+++ Tests/Integration/camera_securitySystem_doggotest.py
@@ -0,0 +1,27 @@
+import unittest
+from datetime import datetime
+from unittest.mock import patch
+from AlertCreators.camera import Camera
+from AlertHandlers.doggo import Doggo
+from security_system import SecuritySystem
+
+class Camera_SecuritySystem_DoggoTest(unittest.TestCase):
+  
+  @patch('builtins.print')
+  def test_cameraAlertCreated_doggoAlertHandled(self, print_mock):
+    # setup
+
+    camera = Camera("Camera1", ["outside", "garage"])
+    doggo = Doggo("Jake")
+    system = SecuritySystem()
+
+    system.registerCreator(camera)
+    system.registerHandler(doggo)
+
+
+    # act
+    camera.detect_movement("human", "outside", datetime(2020, 11, 26, 13, 20))
+
+
+    # assert
+    print_mock.assert_called_once_with("Jake: growl!")
