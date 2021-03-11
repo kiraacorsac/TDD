4ffb271276fb672bc8f9d96d43f87a8a0d39284b
Filka, Ladislav (lf925u) <lf925u@att.com>
2021-03-11 12:06:41 +0100

    Added Test Alert not activated

--- Tests/Integration/camera_securitySystem_doggotest.py
+++ Tests/Integration/camera_securitySystem_doggotest.py
@@ -25,3 +25,21 @@ class Camera_SecuritySystem_DoggoTest(unittest.TestCase):
 
     # assert
     print_mock.assert_called_once_with("Jake: growl!")
+
+
+  @patch('builtins.print')
+  def test_cameraAlertCreated_doggoAlert_not_activated(self, print_mock):
+    # setup
+
+    camera = Camera("Camera1", ["outside", "garage"])
+    doggo = Doggo("Jake")
+    system = SecuritySystem()
+
+    system.registerCreator(camera)
+    system.registerHandler(doggo)
+
+    #act
+    camera.detect_movement("eagle", "outside", datetime(2020, 11, 26, 13, 20))
+    
+    # assert   
+    print_mock.assert_not_called()
