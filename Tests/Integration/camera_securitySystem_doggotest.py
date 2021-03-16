f46973be0442a56fee8ff646c4e07fe32589c765
Gaeschlin, Quentin (qg218x) <qg218x@att.com>
2021-03-16 17:13:26 +0100

    now with integration test ???

--- Tests/Integration/camera_securitySystem_doggotest.py
+++ Tests/Integration/camera_securitySystem_doggotest.py
@@ -3,45 +3,62 @@ from datetime import datetime
 from unittest.mock import patch
 from AlertCreators.camera import Camera
 from AlertHandlers.doggo import Doggo
+from AlertHandlers.sound_alarm import SoundAlarm
 from security_system import SecuritySystem
 from SecuritySystemStrategies.everybody_strategy import EverybodyStrategy
+from SecuritySystemStrategies.oldest_first_strategy import OldestFirstStrategy
+
 
 class Camera_SecuritySystem_DoggoTest(unittest.TestCase):
-  
-  @patch('builtins.print')
-  def test_cameraAlertCreated_doggoAlertHandled(self, print_mock):
-    # setup
+    @patch("builtins.print")
+    def test_cameraAlertCreated_doggoAlertHandled(self, print_mock):
+        # setup
 
-    camera = Camera("Camera1", ["outside", "garage"])
-    doggo = Doggo("Jake")
-    system = SecuritySystem()
-    system.strategy = EverybodyStrategy()
+        camera = Camera("Camera1", ["outside", "garage"])
+        doggo = Doggo("Jake")
+        system = SecuritySystem()
+        system.strategy = EverybodyStrategy()
 
-    system.registerCreator(camera)
-    system.registerHandler(doggo)
+        system.registerCreator(camera)
+        system.registerHandler(doggo)
 
+        # act
+        camera.detect_movement("human", "outside", datetime(2020, 11, 26, 13, 20))
 
-    # act
-    camera.detect_movement("human", "outside", datetime(2020, 11, 26, 13, 20))
+        # assert
+        print_mock.assert_called_once_with("Jake: growl!")
 
+    @patch("builtins.print")
+    def test_oldestFirstSTrategy_doggoAlertHandled(self, print_mock):
+        # setup
 
-    # assert
-    print_mock.assert_called_once_with("Jake: growl!")
+        camera = Camera("Camera1", ["outside", "garage"])
+        doggo = Doggo("Jake")
+        system = SecuritySystem()
+        system.strategy = OldestFirstStrategy()
 
-  
-  @patch('builtins.print')
-  def test_cameraAlertCreated_doggoAlertIgnored(self, print_mock):
-    # setup
+        system.registerCreator(camera)
+        system.registerHandler(doggo)
+        system.registerHandler(SoundAlarm())
 
-    camera = Camera("Camera1", ["outside", "garage"])
-    doggo = Doggo("Jake")
-    system = SecuritySystem()
-    system.strategy = EverybodyStrategy()
+        # act
+        camera.detect_movement("human", "outside", datetime(2020, 11, 26, 13, 20))
 
+        # assert
+        print_mock.assert_called_with("Jake: growl!")
 
-    system.registerCreator(camera)
-    system.registerHandler(doggo)
+    @patch("builtins.print")
+    def test_cameraAlertCreated_doggoAlertIgnored(self, print_mock):
+        # setup
 
-    camera.detect_movement("lizzard", "outside", datetime(2020, 11, 26, 13, 20))
+        camera = Camera("Camera1", ["outside", "garage"])
+        doggo = Doggo("Jake")
+        system = SecuritySystem()
+        system.strategy = EverybodyStrategy()
 
-    print_mock.assert_not_called()
+        system.registerCreator(camera)
+        system.registerHandler(doggo)
+
+        camera.detect_movement("lizzard", "outside", datetime(2020, 11, 26, 13, 20))
+
+        print_mock.assert_not_called()
