64c13bfd46dfa963dcb291d1eb29d5109a147104
Gaeschlin, Quentin (qg218x) <qg218x@att.com>
2021-03-16 16:45:24 +0100

    Second Unitest and class for oldest_first_strategy

--- Tests/Unit/SecuritySystemStrategies/oldest_first_strategytest.py
+++ Tests/Unit/SecuritySystemStrategies/oldest_first_strategytest.py
@@ -8,7 +8,6 @@ from SecuritySystemStrategies.oldest_first_strategy import OldestFirstStrategy
 
 class OldestFirstStrategyTest(unittest.TestCase):
     @patch.object(Doggo, "handle_alert")
-    # @patch.object(SoundAlarm, "handle_alert")
     def test_alertDispatch_2handlers_firstHandlerCalledOnly(
         self, handle_alert_Doggo_mock
     ):
@@ -23,4 +22,19 @@ class OldestFirstStrategyTest(unittest.TestCase):
         # assert
 
         handle_alert_Doggo_mock.assert_called()
-        # handle_alert_SoundAlarm_mock.assert_not_called()
+
+    @patch.object(SoundAlarm, "handle_alert")
+    def test_alertDispatch_2handlers_secondHandlerNotCalled(
+        self, handle_alert_SoundAlarm_mock
+    ):
+        # set up
+        strategy = OldestFirstStrategy()
+        handlers = [Doggo("Jake"), SoundAlarm()]
+        alert = Alert("outside", "human", 2)
+
+        # act
+        strategy.alert_dispatch(alert, handlers)
+
+        # assert
+
+        handle_alert_SoundAlarm_mock.assert_not_called()
