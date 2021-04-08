798079c52b4837237dfca3920885a938d26a549e
Kiraa Corsac <wire150@gmail.com>
2021-04-08 13:16:17 +0200

    everybody strategy test gets multiple handlers

--- Tests/Unit/SecuritySystemStrategies/everybody_strategytest.py
+++ Tests/Unit/SecuritySystemStrategies/everybody_strategytest.py
@@ -8,19 +8,26 @@ from SecuritySystemStrategies.everybody_strategy import EverybodyStrategy
 
 
 class EverybodyStrategyTest(unittest.TestCase):
-    def test_alertDispatch_2handlers_handleAlertCalled2Times(self):
+    def test_alertDispatch_multipleHandlers_handleAlertCalledForEveryHandler(self):
         # set up
         strategy = EverybodyStrategy()
-        handlers = [Mock(name="handler1"), Mock(name="handler2")]
+        handlers = [
+            Mock(name="handler1"),
+            Mock(name="handler2"),
+            Mock(name="handler3"),
+            Mock(name="handler4"),
+            Mock(name="handler5"),
+            Mock(name="handler6"),
+        ]
+
         alert_mock = Mock(name="alert")
         alert_mock.where = "outside"
         alert_mock.what = "human"
-        alert_mock.level = 2 
-        
+        alert_mock.level = 2
+
         # act
         strategy.alert_dispatch(alert_mock, handlers)
 
         # assert
-
-        handlers[0].handle_alert.assert_called_once_with(alert_mock)
-        handlers[1].handle_alert.assert_called_once_with(alert_mock)
+        for handler in handlers:
+            handler.handle_alert.assert_called_once_with(alert_mock)
