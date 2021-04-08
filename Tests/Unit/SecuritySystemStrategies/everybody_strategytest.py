3d722dc0edb45eea5e4fd82376760fd7595c9350
Kiraa Corsac <wire150@gmail.com>
2021-04-08 13:04:02 +0200

    replace concrede instances in everybody strategy test

--- Tests/Unit/SecuritySystemStrategies/everybody_strategytest.py
+++ Tests/Unit/SecuritySystemStrategies/everybody_strategytest.py
@@ -1,26 +1,26 @@
 import unittest
 from unittest.mock import patch, Mock
-from AlertHandlers.doggo import Doggo
-from AlertHandlers.sound_alarm import SoundAlarm
+# from AlertHandlers.doggo import Doggo
+# from AlertHandlers.sound_alarm import SoundAlarm
 from alert import Alert
 from SecuritySystemStrategies.everybody_strategy import EverybodyStrategy
+# from SecuritySystemStrategies.random_strategy import RandomStrategy
 
 
 class EverybodyStrategyTest(unittest.TestCase):
-    @patch.object(Doggo, "handle_alert")
-    @patch.object(SoundAlarm, "handle_alert")
-    def test_alertDispatch_2handlers_handleAlertCalled2Times(
-        self, handle_alert_Doggo_mock, handle_alert_SoundAlarm_mock
-    ):
+    def test_alertDispatch_2handlers_handleAlertCalled2Times(self):
         # set up
         strategy = EverybodyStrategy()
-        handlers = [Doggo("Jake"), SoundAlarm()]
-        alert = Alert("outside", "human", 2)
-
+        handlers = [Mock(name="handler1"), Mock(name="handler2")]
+        alert_mock = Mock(name="alert")
+        alert_mock.where = "outside"
+        alert_mock.what = "human"
+        alert_mock.level = 2 
+        
         # act
-        strategy.alert_dispatch(alert, handlers)
+        strategy.alert_dispatch(alert_mock, handlers)
 
         # assert
 
-        handle_alert_Doggo_mock.assert_called_once()
-        handle_alert_SoundAlarm_mock.assert_called_once()
+        handlers[0].handle_alert.assert_called_once_with(alert_mock)
+        handlers[1].handle_alert.assert_called_once_with(alert_mock)
