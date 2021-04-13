9f720232ebed2a5e1a919a8e94e87190794fc129
Filka, Ladislav (lf925u) <lf925u@att.com>
2021-04-13 12:14:01 +0200

    homework

--- Tests/Unit/SecuritySystemStrategies/oldest_first_strategytest.py
+++ Tests/Unit/SecuritySystemStrategies/oldest_first_strategytest.py
@@ -7,18 +7,28 @@ from SecuritySystemStrategies.oldest_first_strategy import OldestFirstStrategy
 
 
 class OldestFirstStrategyTest(unittest.TestCase):
-    @patch.object(Doggo, "handle_alert")
-    @patch.object(SoundAlarm, "handle_alert")
+#    @patch.object(Doggo, "handle_alert")
+#    @patch.object(SoundAlarm, "handle_alert")
     def test_alertDispatch_oldestFirst(
         self, handle_alert_SoundAlarm_mock, handle_alert_Doggo_mock
     ):
         # set up
         strategy = OldestFirstStrategy()
-        alert = Alert("outside", "human", 2)
-        handlers = [Doggo("Jake"), SoundAlarm()]
+  #      alert = Alert("outside", "human", 2)
+  #      handlers = [Doggo("Jake"), SoundAlarm()]
+
+        handlers = [
+            Mock(name="Doggo"),
+            Mock(name="SoundAlarm"),
+        ]
+
+        alert_mock = Mock(name="alert")
+        alert_mock.where = "outside"
+        alert_mock.what = "human"
+        alert_mock.level = 2
 
         # act
-        strategy.alert_dispatch(alert, handlers)
+        strategy.alert_dispatch(alert_mock, handlers)
 
         # assert
         handle_alert_SoundAlarm_mock.assert_not_called()
